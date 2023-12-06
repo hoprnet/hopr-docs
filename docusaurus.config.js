64839ead525e0c35412fa1dfb83de3eb036bee9e
adharasi <58601773+adharasi@users.noreply.github.com>
2023-12-06 13:46:32 +0000

    Version to 2.05

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -81,7 +81,7 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           routeBasePath: '/',
           editUrl: 'https://github.com/hoprnet/hopr-docs/edit/master/',
-          lastVersion: 'v2.00'
+          lastVersion: 'v2.05'
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css')
