5dd8b17f94b16e18bcde0c9a9844f07244cf04ac
Jaguaras <jaguaras86@gmail.com>
2026-09-14 16:54:19 +0300

    disabling developers section and improving 404, fixing folder location for the docker container

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -26,7 +26,11 @@ const config = {
   url: DOCS_URL,
   baseUrl: '/',
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    },
+  },
   favicon: '/img/hopr_icon.svg',
   organizationName: 'hoprnet',
   projectName: 'hopr-docs',
@@ -56,6 +60,18 @@ const config = {
           editUrl: 'https://github.com/hoprnet/hopr-docs/edit/master/',
           lastVersion: 'v3.0.0',
           onlyIncludeVersions: ['v2.2.3', 'v3.0.0'],
+          // The Developers section is disabled. Files are kept in versioned_docs/
+          // so it can be re-enabled by dropping the 'developers/**' entry below and
+          // restoring the tutorialSidebar entries in the sidebar files.
+          // NOTE: specifying `exclude` replaces Docusaurus' defaults rather than
+          // extending them, so the default patterns are repeated here.
+          exclude: [
+            '**/_*.{js,jsx,ts,tsx,md,mdx}',
+            '**/_*/**',
+            '**/*.test.{js,jsx,ts,tsx}',
+            '**/__tests__/**',
+            'developers/**'
+          ],
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css')
--- versioned_docs/version-v3.0.0/node/node-docker.md
+++ versioned_docs/version-v3.0.0/node/node-docker.md
@@ -89,7 +89,7 @@ Below is a quick reference of all the `hoprd` CLI flags you’ll need to adjust:
 | `--moduleAddress '<MODULE_ADDRESS>'`                        | Your staking Module contract address     |
 | `--host '<YOUR_PUBLIC_IP>:9091'`                            | Your public libp2p endpoint (port 9091)  |
 | `--provider '<CUSTOM_RPC_PROVIDER>'`                        | Gnosis Chain RPC URL (see [Custom RPC provider guide](./custom-rpc-provider.md)).                    |
-| `--configurationFilePath '/app/conf/hoprd-docker.cfg.yaml'` | Path to your custom strategy YAML file   |
+| `--configurationFilePath '/app/hoprd-db/hoprd-docker.cfg.yaml'` | Path to your custom strategy YAML file   |
 
 
 The following settings need to be adjusted in the current Docker command:
